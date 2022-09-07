05935fc03545ec622a3ad924165c0f247218a700
MaryChiodelli <marychiodelli97@gmail.com>
2022-09-07 15:11:31 +0200

    add header

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <img class="header-logo" src="../assets/logo.png" alt="Spotify logo">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'MainHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+  header {
+    padding: 12px 20px;
+    background-color: #2E3A46;
+  }
+
+  .header-logo {
+    width: 46px;
+    aspect-ratio: 1 / 1;
+  }
+</style>
