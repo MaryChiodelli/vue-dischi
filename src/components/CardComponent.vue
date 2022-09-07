0de91fbf3e6089865acb9c28a084a8e88a9399f9
MaryChiodelli <marychiodelli97@gmail.com>
2022-09-07 16:01:59 +0200

    get product data

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <img src="" alt="">
+    <h3>Title</h3>
+    <p>Description</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,42 @@
+<template>
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <CardComponent />
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+  import CardComponent from './CardComponent.vue';
+  import axios from 'axios';
+
+  export default {
+    name: 'MainContent',
+    components: {
+      CardComponent
+    },
+    data() {
+      return {
+        productList: []
+      }
+    },
+    created() {
+      axios
+        .get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then((res) => {
+          this.productList.push(...res.data.response);
+        });
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  main {
+    padding: 5rem 0;
+    background-color: #1E2D3B;
+  }
+</style>
